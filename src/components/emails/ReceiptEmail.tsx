7f0394b1f4fec8ccd07b2bb36edf25bf350cacf3
hermescati <hermescati99@gmail.com>
2024-10-10 23:11:03 +0200

    created the template for the email receipt that users will get

---
+++ src/components/emails/ReceiptEmail.tsx
@@ -0,0 +1,35 @@
+import { Order, Product, User } from "../../types/payload";
+import { render } from "@react-email/components";
+
+import * as React from "react";
+
+interface ReceiptEmailProps {
+  orderId: Order["id"];
+  email: User["email"];
+  date: Date;
+  products: Product[];
+}
+
+export const ReceiptEmail = ({
+  email,
+  date,
+  orderId,
+  products,
+}: ReceiptEmailProps) => {
+  const total = products.reduce((acc, curr) => acc + curr.retail_price, 0) + 1;
+
+  // Create a template from react email
+  return (
+    <>
+      <p>{total}</p>
+      <p>{email}</p>
+      <p>{date.toISOString()}</p>
+      <p>{orderId}</p>
+    </>
+  );
+};
+
+export const ReceiptEmailHtml = (props: ReceiptEmailProps) =>
+  render(<ReceiptEmail {...props} />, {
+    pretty: true,
+  });
