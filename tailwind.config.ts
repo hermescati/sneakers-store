37f35b0d82679ff84ba932c2bc302d674e75608e
hermescati <hermescati99@gmail.com>
2024-10-12 12:26:50 +0200

    small changes to the tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,59 +1,59 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       screens: {
-        "2xl": "1440px",
-        "3xl": "1536px",
+        '2xl': '1440px',
+        '3xl': '1536px'
       },
       colors: {
-        background: "#ffffff",
-        foreground: "#212427",
+        background: '#ffffff',
+        foreground: '#212427',
         primary: {
-          50: "#ffffff",
-          100: "#f9fafb",
-          200: "#f2f2f2",
-          300: "#ebebeb",
-          400: "#bebfc0",
-          500: "#9f9f9f",
-          600: "#6f6f6f",
-          700: "#565656",
-          800: "#33383c",
-          900: "#212427",
+          50: '#ffffff',
+          100: '#f9fafb',
+          200: '#f2f2f2',
+          300: '#ebebeb',
+          400: '#bebfc0',
+          500: '#9f9f9f',
+          600: '#6f6f6f',
+          700: '#565656',
+          800: '#33383c',
+          900: '#212427'
         },
-        secondary: "#fd844a",
-        success: "#15f37d",
-        danger: "#f32f15",
+        secondary: '#fd844a',
+        success: '#15f37d',
+        danger: '#f32f15'
       },
       fontSize: {
-        xs: "0.5rem",
-        sm: "0.75rem",
-        md: "0.825rem",
-        base: "1rem",
-        xl: "1.25rem",
-        "2xl": "1.5rem",
-        "3xl": "2rem",
-        "4xl": "2.5rem",
-        "5xl": "3rem",
+        xs: '0.5rem',
+        sm: '0.75rem',
+        md: '0.825rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '2rem',
+        '4xl': '2.5rem',
+        '5xl': '3rem'
       },
       keyframes: {
         rotate: {
-          "0%": { transform: "rotate(0deg)" },
-          "50%": { transform: "rotate(180deg)" },
-          "100%": { transform: "rotate(360deg)" },
-        },
+          '0%': { transform: 'rotate(0deg)' },
+          '50%': { transform: 'rotate(180deg)' },
+          '100%': { transform: 'rotate(360deg)' }
+        }
       },
       animation: {
-        "spin-180": "rotate 4s ease-in-out infinite",
-      },
-    },
+        'spin-180': 'rotate 4s ease-in-out infinite'
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
