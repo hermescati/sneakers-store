19f5c4c0137d52cbfcbf95d801dfbb1a238538c4
hermescati <hermescati99@gmail.com>
2024-10-18 20:05:45 +0200

    a new util function to darken colors

---
+++ src/utils/colors.ts
@@ -0,0 +1,87 @@
+export const darkenHexColor = (hex: string, percentage: number) => {
+  const r = parseInt(hex.slice(1, 3), 16)
+  const g = parseInt(hex.slice(3, 5), 16)
+  const b = parseInt(hex.slice(5, 7), 16)
+
+  const rgbToHsl = (r: number, g: number, b: number) => {
+    r /= 255
+    g /= 255
+    b /= 255
+
+    const max = Math.max(r, g, b)
+    const min = Math.min(r, g, b)
+    let h = 0,
+      s = 0,
+      // eslint-disable-next-line prefer-const
+      l = (max + min) / 2
+
+    if (max !== min) {
+      const delta = max - min
+      s = l > 0.5 ? delta / (2 - max - min) : delta / (max + min)
+
+      switch (max) {
+        case r:
+          h = (g - b) / delta + (g < b ? 6 : 0)
+          break
+        case g:
+          h = (b - r) / delta + 2
+          break
+        case b:
+          h = (r - g) / delta + 4
+          break
+      }
+      h /= 6
+    }
+    return { h: h * 360, s: s * 100, l: l * 100 }
+  }
+
+  const { h, s, l } = rgbToHsl(r, g, b)
+
+  const newL = Math.max(0, l - percentage)
+
+  const hslToRgb = (h: number, s: number, l: number) => {
+    const c = (1 - Math.abs(2 * l - 1)) * s
+    const x = c * (1 - Math.abs(((h / 60) % 2) - 1))
+    const m = l - c / 2
+    let r = 0,
+      g = 0,
+      b = 0
+
+    if (h < 60) {
+      r = c
+      g = x
+      b = 0
+    } else if (h < 120) {
+      r = x
+      g = c
+      b = 0
+    } else if (h < 180) {
+      r = 0
+      g = c
+      b = x
+    } else if (h < 240) {
+      r = 0
+      g = x
+      b = c
+    } else if (h < 300) {
+      r = x
+      g = 0
+      b = c
+    } else {
+      r = c
+      g = 0
+      b = x
+    }
+
+    return [
+      Math.round((r + m) * 255),
+      Math.round((g + m) * 255),
+      Math.round((b + m) * 255)
+    ]
+  }
+
+  const [newR, newG, newB] = hslToRgb(h, s / 100, newL / 100)
+
+  const toHex = (n: number) => n.toString(16).padStart(2, '0')
+  return `#${toHex(newR)}${toHex(newG)}${toHex(newB)}`
+}
