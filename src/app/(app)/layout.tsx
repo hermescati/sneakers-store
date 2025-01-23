4a5397db2c70849d7a1eda63b830023219290203
hermescati <hermescati99@gmail.com>
2025-01-23 18:02:02 +0100

    enabled vercel speed insights and analytics

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,8 @@
 import Footer from '@/components/nav/Footer'
 import Navbar from '@/components/nav/Navbar'
 import { cn } from '@/utils'
+import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata } from 'next'
 import { Quicksand } from 'next/font/google'
 import { Toaster } from 'sonner'
@@ -21,11 +23,13 @@ const RootLayout = ({
   return (
     <html lang="en" className="h-full">
       <body className={cn('relative h-full antialiased', quicksand.className)}>
-          <main className="relative flex flex-col min-h-screen">
-            <Navbar />
-            <div className="flex flex-1 justify-center">{children}</div>
-            <Footer />
-          </main>
+        <main className="relative flex flex-col min-h-screen">
+          <Navbar />
+          <div className="flex flex-1 justify-center">{children}</div>
+          <Footer />
+        </main>
+        <Analytics />
+        <SpeedInsights />
         <Toaster position="top-center" richColors />
       </body>
     </html>
