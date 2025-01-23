db6ccc7a67975da8fe41ac7c14f5b046ccadd971
hermescati <hermescati99@gmail.com>
2025-01-23 11:55:49 +0100

    deleted migrations folder again

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250123_104438_initial_migration from './20250123_104438_initial_migration';
-
-export const migrations = [
-  {
-    up: migration_20250123_104438_initial_migration.up,
-    down: migration_20250123_104438_initial_migration.down,
-    name: '20250123_104438_initial_migration'
-  },
-];
