a65469143a6673519c796ccd4d238a02473830f4
hermescati <hermescati99@gmail.com>
2025-03-12 16:25:27 +0100

    added a filter control to wrap all filter components with the same style

--- src/components/base/input/MultiSelect.tsx
+++ src/components/base/input/MultiSelect.tsx
@@ -1,10 +1,8 @@
 'use client'
 
+import FilterControl from "@/components/filters/FilterControl"
 import { SelectOption } from "@/types"
-import { cn } from "@/utils"
-import { Icon } from '@iconify/react'
-import { useRef, useState } from "react"
-import { useOnClickOutside } from "usehooks-ts"
+import { useState } from "react"
 import SelectMenu from "./SelectMenu"
 
 interface MultiSelectProps {
@@ -20,11 +18,7 @@ const MultiSelect = ({
     placeholder = 'Select a value',
     onChange
 }: MultiSelectProps) => {
-    const [isExpanded, setIsExpanded] = useState(false)
     const [selectedOptions, setSelectedOptions] = useState<string[]>([])
-
-    const multiSelectRef = useRef<HTMLDivElement>(null!)
-    useOnClickOutside(multiSelectRef, () => setIsExpanded(false))
 
     const handleSelectionChange = (selectedOptions: string[]) => {
         setSelectedOptions(selectedOptions)
@@ -32,44 +26,31 @@ const MultiSelect = ({
     }
 
     return (
-        <div ref={multiSelectRef} className='relative'>
-            <div
-                id={id}
-                aria-label='toggle menu'
-                aria-haspopup='true'
-                aria-expanded={isExpanded}
-                className='flex items-center justify-between gap-3 w-full px-3 py-2 rounded-xl border border-border cursor-pointer'
-                onClick={() => setIsExpanded((prev) => !prev)}
-            >
-                <div className="flex-1 overflow-hidden text-ellipsis line-clamp-1">
-                    {selectedOptions.length > 0
-                        ? (
-                            <p className="font-medium text-md">
-                                {selectedOptions
-                                    .map((value) => {
-                                        const option = options.find((opt) => opt.value === value)
-                                        return option ? option.label : ''
-                                    })
-                                    .join(', ')}
-                            </p>
-                        )
-                        : <p className="text-md font-medium text-primary-600">{placeholder}</p>
-                    }
-                </div>
-                <button className='p-1 text-primary-600'>
-                    <Icon icon='mage:chevron-down' className={cn('transition-all duration-300 ease-in-out', { 'rotate-180': isExpanded })} />
-                </button>
-            </div>
-
-            {isExpanded &&
+        <FilterControl
+            id={id}
+            menu={
                 <SelectMenu
                     options={options}
                     selectId={id}
                     multiple
                     selected={selectedOptions}
                     onSelect={handleSelectionChange}
-                    onClear={() => setSelectedOptions([])} />}
-        </div>
+                    onClear={() => setSelectedOptions([])} />
+            }>
+            {selectedOptions.length > 0
+                ? (
+                    <p className="font-medium text-md">
+                        {selectedOptions
+                            .map((value) => {
+                                const option = options.find((opt) => opt.value === value)
+                                return option ? option.label : ''
+                            })
+                            .join(', ')}
+                    </p>
+                )
+                : <p className="text-md font-medium text-primary-600">{placeholder}</p>
+            }
+        </FilterControl >
     )
 }
 
