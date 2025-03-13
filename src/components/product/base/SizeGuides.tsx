3e1fa2ef0dc2cb07629f43bddcd362b8aecf1cb1
hermescati <hermescati99@gmail.com>
2025-03-13 13:34:06 +0100

    updated size guides table component

--- src/components/product/base/SizeGuides.tsx
+++ src/components/product/base/SizeGuides.tsx
@@ -1,64 +1,34 @@
-export const SIZE_CATEGORIES: Record<string, number[]> = {
-    "US Men": [
-        3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5,
-        11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18
-    ],
-    "US Women": [
-        5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5,
-        13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18, 18.5, 19, 19.5
-    ],
-    "UK": [
-        3, 3.5, 4, 4.5, 5, 5.5, 6, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10,
-        10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17
-    ],
-    "Europe": [
-        35.5, 36, 36.5, 37.5, 38, 38.5, 39, 40, 40.5, 41, 42, 42.5, 43, 44,
-        44.5, 45, 45.5, 46, 47, 47.5, 48, 48.5, 49, 49.5, 50, 50.5, 51, 51.5,
-        52, 52.5
-    ],
-    "Foot Length (cm)": [
-        22.5, 23, 23.5, 23.5, 24, 24, 24.5, 25, 25.5, 26, 26.5, 27, 27.5, 28,
-        28.5, 29, 29.5, 30, 30.5, 31, 31.5, 32, 32.5, 33, 33.5, 34, 34.5, 35,
-        35.5, 36
-    ],
-    "Foot Length (in)": [
-        8.9, 9.1, 9.3, 9.3, 9.4, 9.4, 9.6, 9.8, 10, 10.2, 10.4, 10.6, 10.8,
-        11, 11.2, 11.4, 11.6, 11.8, 12, 12.2, 12.4, 12.6, 12.8, 13, 13.2, 13.4,
-        13.6, 13.8, 14, 14.2
-    ]
-}
+import { SIZE_CATEGORIES } from '@/lib/options'
 
 const SizeGuides = () => {
-    return (
-        <table className="table-auto md:table-fixed sm:mr-2 mb-1 border-separate border-spacing-0">
-            <thead className="sticky top-0 z-10 bg-primary-100">
-                <tr>
-                    {Object.keys(SIZE_CATEGORIES).map((category, index) => (
-                        <th
-                            key={index}
-                            className="px-5 py-2 text-md text-center font-medium text-primary-700 border-y border-r first:border-l border-primary-300 dark:border-border"
-                        >
-                            {category}
-                        </th>
-                    ))}
-                </tr>
-            </thead>
+    const filteredCategories = Object.keys(SIZE_CATEGORIES).filter(category => category !== "US Kids")
 
-            <tbody>
-                {SIZE_CATEGORIES["US Men"].map((_, index) => (
-                    <tr key={index} className="hover:bg-primary-100/50 transition-colors duration-150 ease-in-out">
-                        {Object.keys(SIZE_CATEGORIES).map((category, idx) => (
-                            <td
-                                key={idx}
-                                className="px-5 py-2 text-md text-center font-medium text-primary-700 border-b border-r first:border-l border-primary-300 dark:border-border"
-                            >
-                                {SIZE_CATEGORIES[category][index] || '-'}
-                            </td>
-                        ))}
-                    </tr>
+    return (
+        <div className="min-w-max md:min-w-0 w-full">
+            <div className="sticky top-0 inset-x-0 grid grid-cols-6 bg-primary-900 z-10">
+                {filteredCategories.map((category, index) => (
+                    <div
+                        key={index}
+                        className="place-content-center p-2 border-l border-primary-300 dark:border-border text-md text-center font-medium text-background"
+                    >
+                        {category}
+                    </div>
                 ))}
-            </tbody>
-        </table>
+            </div>
+
+            {SIZE_CATEGORIES["US Mens"].map((_, index) => (
+                <div key={index} className="grid grid-cols-6 border-primary-300 dark:border-border hover:bg-primary-100/50">
+                    {filteredCategories.map((category, idx) => (
+                        <div
+                            key={idx}
+                            className="place-content-center p-2 border-b border-l last:border-r border-primary-300 dark:border-border text-md text-center font-medium text-primary-700"
+                        >
+                            {SIZE_CATEGORIES[category][index] || '-'}
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </div>
     )
 }
 
--- src/lib/options.ts
+++ src/lib/options.ts
@@ -12,8 +12,41 @@ export const DISCOUNT_TYPE_OPTIONS: SelectOption[] = [
 ]
 
 export const SORT_OPTIONS: SelectOption[] = [
-    { label: 'New Releases', value: 'release_date|desc', icon: 'mage:arrow-down' },
-    { label: 'Older Releases', value: 'release_date|asc', icon: 'mage:arrow-up' },
-    { label: 'Lowest Price', value: 'min_price|asc', icon: 'mage:arrow-up' },
-    { label: 'Highest Price', value: 'min_price|desc', icon: 'mage:arrow-down' }
+    { label: 'New Releases', value: 'release_date|desc', icon: 'hugeicons:sort-by-down-01' },
+    { label: 'Older Releases', value: 'release_date|asc', icon: 'hugeicons:sort-by-up-01' },
+    { label: 'Highest Price', value: 'min_price|desc', icon: 'hugeicons:sorting-19' },
+    { label: 'Lowest Price', value: 'min_price|asc', icon: 'hugeicons:sorting-91' },
 ]
+
+export const SIZE_CATEGORIES: Record<string, number[]> = {
+    "US Mens": [
+        3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5,
+        11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18
+    ],
+    "US Womens": [
+        5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5,
+        13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18, 18.5, 19, 19.5
+    ],
+    "US Kids": [
+        0, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7
+    ],
+    "UK": [
+        3, 3.5, 4, 4.5, 5, 5.5, 6, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10,
+        10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17
+    ],
+    "Europe": [
+        35.5, 36, 36.5, 37.5, 38, 38.5, 39, 40, 40.5, 41, 42, 42.5, 43, 44,
+        44.5, 45, 45.5, 46, 47, 47.5, 48, 48.5, 49, 49.5, 50, 50.5, 51, 51.5,
+        52, 52.5
+    ],
+    "Foot Length (cm)": [
+        22.5, 23, 23.5, 23.5, 24, 24, 24.5, 25, 25.5, 26, 26.5, 27, 27.5, 28,
+        28.5, 29, 29.5, 30, 30.5, 31, 31.5, 32, 32.5, 33, 33.5, 34, 34.5, 35,
+        35.5, 36
+    ],
+    "Foot Length (in)": [
+        8.9, 9.1, 9.3, 9.3, 9.4, 9.4, 9.6, 9.8, 10, 10.2, 10.4, 10.6, 10.8,
+        11, 11.2, 11.4, 11.6, 11.8, 12, 12.2, 12.4, 12.6, 12.8, 13, 13.2, 13.4,
+        13.6, 13.8, 14, 14.2
+    ]
+}
