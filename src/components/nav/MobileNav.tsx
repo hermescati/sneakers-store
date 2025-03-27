95d67796bf1d3928b1d90a28758cdcdc934ae940
hermescati <hermescati99@gmail.com>
2025-03-27 23:07:53 +0100

    refactor: swapped mobile navbar for drawer

--- src/components/nav/MobileNav.tsx
+++ src/components/nav/MobileNav.tsx
@@ -1,167 +1,149 @@
 'use client'
 
 import useNavigationMenu from '@/hooks/useNavigationMenu'
-import useOnKeyPress from '@/hooks/useOnKeyPress'
 import { useCartStore } from '@/stores/cartStore'
 import { useUserStore } from '@/stores/userStore'
 import { NavItem } from '@/types'
 import { cn } from '@/utils'
 import { Icon } from '@iconify/react'
 import { usePathname, useRouter } from 'next/navigation'
-import { useEffect, useRef, useState } from 'react'
-import { useOnClickOutside } from 'usehooks-ts'
+import { useEffect, useState } from 'react'
+import { Drawer } from 'vaul'
 import { AccordionItem } from '../base/Accordion'
-import Link from '../base/Link'
 import Button from '../base/button/Button'
 import IconButton from '../base/button/IconButton'
+import Link from '../base/Link'
 
-interface MobileNavProps {
-  items: NavItem[]
-}
+const MobileNav = ({ navLinks }: { navLinks: NavItem[] }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [openIndex, setOpenIndex] = useState<number | null>(null)
 
-// TODO: Add a sliding animation to it
-// TODO: Clean up the code - break it down to smaller components
-const MobileNav = ({ items }: MobileNavProps) => {
-  const router = useRouter()
-  const pathname = usePathname()
-
+  const menuItems = useNavigationMenu()
   const { items: cartItems } = useCartStore()
   const { user } = useUserStore()
 
-  const menuItems = useNavigationMenu()
-
-  const [openIndex, setOpenIndex] = useState<number | null>(null)
-  const [isOpen, setIsOpen] = useState<boolean>(false)
-
-  const navRef = useRef<HTMLDivElement>(null!)
-  useOnKeyPress({ key: 'm', ctrl: true, preventDefault: true }, () => setIsOpen(true))
-  useOnClickOutside(navRef, () => setIsOpen(false))
+  const router = useRouter()
+  const pathname = usePathname()
 
   useEffect(() => {
     setIsOpen(false)
   }, [pathname])
-
-  useEffect(() => {
-    if (isOpen) {
-      document.body.classList.add('overflow-hidden')
-    } else {
-      document.body.classList.remove('overflow-hidden')
-    }
-  }, [isOpen])
 
   const closeOnCurrent = (href: string) => {
     router.push(href)
     setIsOpen(false)
   }
 
-  const handleToggle = (index: number) => {
-    setOpenIndex(openIndex === index ? null : index)
-  }
-
-  if (!isOpen) {
-    return <IconButton
-      icon="solar:hamburger-menu-linear"
-      onClick={() => setIsOpen(true)}
-      className="p-1.5 text-[1.75rem]" />
-  }
+  const navLinkClasses = "flex items-center justify-between px-4 py-3.5 rounded-lg font-semibold transition-all duration-300 ease-in-out"
 
   return (
-    <>
-      {/* TODO: Use the drawer here also */}
-      <div className="relative z-40 lg:hidden">
-        <div className="fixed inset-0 bg-black bg-opacity-35" />
+    <Drawer.Root direction='right' open={isOpen} onOpenChange={setIsOpen}>
+      <Drawer.Trigger asChild>
+        <IconButton icon="solar:hamburger-menu-linear" className="p-1.5 text-[1.75rem]" />
+      </Drawer.Trigger>
+      <Drawer.Portal>
+        <Drawer.Overlay className='fixed inset-0 z-30 bg-black/40' />
+        <Drawer.Content
+          className="flex fixed right-0 inset-y-2 z-30 w-[85%] max-w-lg rounded-l-2xl border-l border-y border-border bg-background shadow-xl outline-none overflow-hidden"
+          style={{ '--initial-transform': 'calc(100% + 3rem)' } as React.CSSProperties}>
+          <div className="relative flex flex-col h-full grow">
+            <Drawer.Title></Drawer.Title>
 
-        <div className="fixed inset-y-1 right-0 z-40 flex justify-end w-4/5">
-          <div ref={navRef} className="relative w-full h-full flex flex-col border border-border rounded-s-xl overflow-y-auto shadow-xl bg-background">
-            <div className="flex items-center justify-between flex-none px-4 py-2">
-              <IconButton
-                icon="tabler-x"
-                className="text-2xl"
-                onClick={() => setIsOpen(false)} />
-              <div className='relative'>
+            <div className='flex flex-col divide-y divide-border w-full h-full'>
+              <div className="flex flex-none px-4 py-2 items-center justify-between">
                 <IconButton
-                  href="/cart"
-                  icon="solar:cart-large-minimalistic-linear"
-                  className="text-primary-700 text-2xl" />
-                {cartItems.length > 0 && <span className='absolute top-[20%] right-[15%] w-2 h-2 rounded-full bg-secondary' />}
+                  icon="tabler-x"
+                  className="text-2xl"
+                  onClick={() => setIsOpen(false)} />
+                <div className='relative'>
+                  <IconButton
+                    href="/cart"
+                    icon="solar:cart-large-minimalistic-linear"
+                    className="text-primary-700 text-2xl" />
+                  {cartItems.length > 0 && <span className='absolute top-[20%] right-[15%] w-2 h-2 rounded-full bg-secondary' />}
+                </div>
               </div>
-            </div>
 
-            <div className='flex-1 overflow-y-auto px-3 border-y border-border divide-y divide-border'>
-              {!!menuItems.length && <ul className="py-2">
-                {menuItems.map((item) => (
-                  <li key={item.value}>
+              <div className='flex flex-col gap-2 h-full overflow-y-auto'>
+                {menuItems.length > 0 && <ul className="mx-3 mt-2 space-y-0.5">
+                  {menuItems.map((item) => (
                     <Link
+                      key={item.value}
                       href={item.route}
                       onClick={item.action}
                       className={cn(
-                        "flex items-center justify-between py-3 px-4 rounded-lg transition-all duration-300 ease-in-out",
+                        navLinkClasses,
                         {
                           "hover:bg-primary-100": !!item.route || item.action,
-                          "py-1": !!item.subtitle || item.component
+                          "py-1.5": !!item.subtitle || item.component
                         }
                       )}>
-                      <div className="flex flex-col">
-                        <span className="font-medium">{item.title}</span>
-                        {item.subtitle && <span className="font-medium text-md text-primary-600">{item.subtitle}</span>}
+                      <div className="leading-snug">
+                        <p className="font-semibold">{item.title}</p>
+                        {item.subtitle && <p className="font-medium text-sm text-primary-600">{item.subtitle}</p>}
                       </div>
                       {item.icon && <Icon icon={item.icon} className="text-xl" />}
                       {item.component && item.component}
                     </Link>
-                  </li>
-                ))}
-              </ul>}
+                  ))}
+                </ul>}
 
-              <ul className="py-2">
-                {items.map((item, index) => (
-                  <li key={item.name}>
-                    {item.items?.length ?
-                      <AccordionItem
-                        title={item.name}
-                        titleClass="px-4 py-3 rounded-lg font-medium"
-                        iconClass='w-5 h-5'
-                        isOpen={openIndex === index}
-                        onOpen={() => handleToggle(index)}>
-                        <ul className='grid grid-cols-2 md:grid-cols-3 gap-x-8 md:gap-0 px-8 mt-1 mb-2'>
-                          {item.items.map((ft) =>
-                            <Link
-                              key={ft.name}
-                              underline
-                              onClick={() => closeOnCurrent(ft.href!)}
-                              className='w-fit py-0.5 font-medium text-md text-primary-600'>
-                              {ft.name}
-                            </Link>
-                          )}
-                        </ul>
-                      </AccordionItem> :
-                      <Link
-                        onClick={() => closeOnCurrent(item.href!)}
-                        className={cn(
-                          "flex items-center justify-between py-3 px-4 rounded-lg font-medium transition-all duration-300 ease-in-out",
-                          {
-                            "hover:bg-primary-100": item.href,
-                            "text-secondary hover:text-secondary": item.name === 'On Sale'
-                          })}>
-                        {item.name}
-                      </Link>
-                    }
-                  </li>
-                ))}
-              </ul>
-            </div>
+                <span className='w-full h-px border-t border-border' />
 
-            {!user &&
-              <div className="py-3 px-4">
-                <Button
-                  href="/login"
-                  label="Sign in"
-                  className="w-full" />
+                <ul className="mx-3 space-y-0.5">
+                  {navLinks.map((link, index) => (
+                    <li key={link.name}>
+                      {link.items?.length ?
+                        <AccordionItem
+                          title={link.name}
+                          titleClass={navLinkClasses}
+                          iconClass='w-5 h-5'
+                          isOpen={openIndex === index}
+                          onOpen={() => setOpenIndex(prev => (prev === index ? null : index))}
+                        >
+                          <ul className='grid grid-cols-2 gap-x-6 gap-y-3 p-4 border-b border-border'>
+                            {link.items.map((ft) =>
+                              <Link
+                                key={ft.name}
+                                underline
+                                onClick={() => closeOnCurrent(ft.href!)}
+                                className='w-fit font-medium text-md text-primary-700'>
+                                {ft.name}
+                              </Link>
+                            )}
+                          </ul>
+                        </AccordionItem>
+                        : <Link
+                          underline
+                          onClick={() => closeOnCurrent(link.href!)}
+                          className={cn(
+                            navLinkClasses,
+                            {
+                              "hover:bg-primary-100": link.href,
+                              "text-secondary hover:text-secondary": link.name === 'On Sale'
+                            })}>
+                          {link.name}
+                        </Link>
+                      }
+                    </li>
+                  ))}
+                </ul>
               </div>
-            }
+
+              {!user
+                ? <div className="py-3 px-4">
+                  <Button
+                    href="/login"
+                    label="Sign in"
+                    className="w-full" />
+                </div>
+                : <div className='!border-t-0 bg-background h-3' />
+              }
+            </div>
           </div>
-        </div>
-      </div>
-    </>
+        </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
   )
 }
 
