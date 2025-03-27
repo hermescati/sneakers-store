06b01542a455ce35be499532bbbe8793a5ba3abb
hermescati <hermescati99@gmail.com>
2025-03-27 23:07:53 +0100

    refactor: swapped mobile navbar for drawer

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -10,28 +10,26 @@ const Navbar = async () => {
   const navItems = await getNavLinks()
 
   return (
-    <nav className="sticky z-20 top-0 inset-x-0">
-      <div className="relative bg-background shadow dark:border-b dark:border-border">
-        <MainContainer>
-          <div className="flex gap-16 py-3 lg:py-4 lg:pt-6 lg:pb-4 items-center justify-between">
-            <Link href="/" className="font-bold text-xl text-foreground">Sneakers.</Link>
-            <div className="flex items-center gap-3 lg:flex-grow lg:gap-16">
-              <SearchDrawer />
-              <div className='hidden lg:block'>
+    <nav className="sticky z-20 top-0 inset-x-0 bg-background shadow dark:border-b dark:border-border/50">
+      <MainContainer>
+        <div className="flex items-center justify-between gap-6 lg:gap-16 py-3 lg:py-4 lg:pt-6 lg:pb-4">
+          <Link href="/" className="font-bold text-xl text-foreground" aria-label='Home - Sneakers'>
+            Sneakers.
+          </Link>
+          <div className="flex items-center gap-3 lg:flex-grow lg:gap-16 divide-x divide-border lg:divide-x-0">
+            <SearchDrawer />
+            <div className="flex items-center gap-3">
+              <div className="hidden lg:flex">
                 <UserNav />
               </div>
-              <span
-                className="lg:hidden h-8 w-px bg-border"
-                aria-hidden="true"
-              />
-              <div className='lg:hidden'>
-                <MobileNav items={navItems} />
+              <div className="lg:hidden ml-3">
+                <MobileNav navLinks={navItems} />
               </div>
             </div>
           </div>
-          <NavLinks items={navItems} />
-        </MainContainer>
-      </div>
+        </div>
+        <NavLinks items={navItems} />
+      </MainContainer>
     </nav>
   )
 }
